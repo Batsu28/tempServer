db70c38a2b2704447163c31cc8056b223b9beb6a
Morning <morning@admins-iMac.local>
2023-01-26 12:36:22 +0800

    delete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+const cors = require("cors");
 const express = require("express");
-var cors = require("cors");
+const { v1: uuidv1 } = require("uuid");
 
 const app = express();
 const port = 2000;
@@ -8,29 +9,37 @@ const data = require("./data.json");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.get("/products", (req, res) => {
-	console.log("product-iin req irlee"), res.status(200).json(data.products);
+  console.log("product-iin req irlee"), res.status(200).json(data.products);
 });
 app.get("/logInUser", (req, res) => {
-	console.log("logInUsers-iin req irlee"), res.status(200).json(data.logInUser);
+  console.log("logInUsers-iin req irlee"), res.status(200).json(data.logInUser);
 });
 app.get("/orders", (req, res) => {
-	console.log("users-iin req irlee"), res.status(200).json(data.orders);
+  console.log("users-iin req irlee"), res.status(200).json(data.orders);
 });
 
-app.get("/users", (req, res) => {
-	console.log("users req"), res.status(200).json(data.users);
+app.post("/products", (req, res) => {
+  data.products.push({ ...req.body, id: uuidv1().split("-")[0] });
+  res.status(201).send("uploaded");
+  res.status(403).send("error403");
 });
-app.post("/users", (req, res) => {
-	const user = req.body;
-	data.users.push(user);
-	res.status(201).send("uploaded");
-	res.status(403).send("error403");
-	res.status(404).send("error404");
+app.get("/test", (req, res) => {
+  console.log("test req irlee"), res.status(200).json(data.test);
+});
+app.post("/test", (req, res) => {
+  console.log(req.body);
+  data.test.push({ ...req.body, id: uuidv1() });
+  res.status(201).send("uploaded");
+  res.status(403).send("error403");
 });
 
+app.delete("/test/:id", (req, res) => {
+  const filter = data.test.filter((one) => one.id !== req.params.id);
+  data.test = filter;
+  res.send(`id: ${req.params.id} deleted successfully`);
+});
 app.listen(port, () => {
-	console.log(`server is starting in ${port} port`);
+  console.log(`server is starting in ${port} port`);
 });
